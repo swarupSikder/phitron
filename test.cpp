dce5ca8784eb4087b8bcf14294a6ac811c59f20f
swarupSikder <swarupfx20@gmail.com>
2024-11-25 16:54:46 +0600

    001

---
+++ test.cpp
@@ -0,0 +1,14 @@
+//author:   Swarup Sikder
+//judge:    ...
+//problem:  ...
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "hello";
+    for (char ch : s) {
+        cout << ch << " ";
+    }
+    
+    return 0;
+}
